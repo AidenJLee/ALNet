7ca8095c4f981aa575eff0abc45ed281167f0df1
AidenJLee <entist@me.com>
2013-11-18 15:45:25 +0900

    init project

---
+++ ALNet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ALNet
+//
+//  Created by HoJun Lee on 2013. 11. 18..
+//  Copyright (c) 2013년 HoJun Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ALNet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ALNet
+//
+//  Created by HoJun Lee on 2013. 11. 18..
+//  Copyright (c) 2013년 HoJun Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

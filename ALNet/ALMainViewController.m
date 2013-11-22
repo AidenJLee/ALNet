2e76aa0b0d968cd2c8ab06136930bd0792b4f868
AidenJLee <ipory.JL@gmail.com>
2013-11-22 14:38:21 +0900

    add work test sample

--- ALNet/ALMainViewController.m
+++ ALNet/ALMainViewController.m
@@ -77,6 +77,98 @@
                                    }];
 }
 
+- (IBAction)sendPostForMakeGame:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/makegame",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"creatorid": @"528d25bf8055296c3a000001",
+                                           @"opponentid": @"528d26c9453b5e973a000001",
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForStartGame:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/startgame",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"gameid": @"528d9f8801eaf03c3d000001",
+                                           @"opponentid": @"528d25bf8055296c3a000001"
+                                           // 게임 시작이니까 게임 만든 사람 아이디 ( 이정보는 노티로 전송 됨 )
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForCompleteTurn:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/completeturn",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"openidid": @"1412523515",
+                                           @"openidtype": @"FACEBOOK"
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForCompleteRound:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/completeround",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"openidid": @"1412523515",
+                                           @"openidtype": @"FACEBOOK"
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForStartRound:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/startround",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"openidid": @"1412523515",
+                                           @"openidtype": @"FACEBOOK"
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForCompleteGame:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/completegame",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"openidid": @"1412523515",
+                                           @"openidtype": @"FACEBOOK"
+                                           }
+                                   }];
+}
+
+- (IBAction)sendPostForGameResult:(id)sender {
+    [_alt sendRequestForUserInfo:@{
+                                   @"url"        : @"http://ec2-54-238-212-83.ap-northeast-1.compute.amazonaws.com:8080/v1/games/gameresult",
+                                   @"httpMethod" : @"post",
+                                   @"type"       : @"json",
+                                   @"param"  : @{
+                                           @"appid": @"asfdsfa",
+                                           @"openidid": @"1412523515",
+                                           @"openidtype": @"FACEBOOK"
+                                           }
+                                   }];
+}
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
